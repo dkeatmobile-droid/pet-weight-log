82fad823a8f5ce40c991fb4a78cf12c47b13faaa
dkeatmobile-droid <dkeatmobile@gmail.com>
2026-03-29 18:42:20 +0100

    Update service worker cache and files
    Updated cache version and added additional files to cache.

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
+const CACHE_NAME = 'catfatfighters-v3';   // increase version when you make big changes
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('fatfighters-v1').then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         '/',
-        '/index.html'
-        // add more files if you have them
+        '/index.html',
+        '/style.css',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        // Add any other images or files you have
       ]);
     })
   );
@@ -14,6 +20,17 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
